99f80fbf19be2d936ad6431aad86bbac9fedf592
olamideazeez <65969103+olamideazeez@users.noreply.github.com>
2022-10-29 09:48:34 +0100

    node demo commit

---
+++ node demo portfolio/index.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const fs = require('fs')
+const http = require('http')
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
